ab46b5dd300ec0f4b0daef5727fa141ff4446ff4
Andi-00 <andreasweber2001@outlook.de>
2023-08-07 21:57:54 +0200

    code

--- Network/network.py
+++ Network/network.py
@@ -2,7 +2,42 @@ from tensorflow import keras
 import numpy as np
 from matplotlib import pyplot as plt
 
-# Input data : shape 79 x 2001 (time x frequency)
+# Input data : shape 2001 x 79 (frequency x time)
+
+# Load the data
+
+data = []
+
+# Number of datasets with n_max = 1E4
+n_data = 10
+
+# Templates for the data sets and the true values
+data = np.zeros((n_data, 79, 2001))
+labels = np.zeros((n_data, 5))
+
+# loading the data sets and the labels
+for i in range(n_data):
+    tempd = np.genfromtxt("/hpcwork/cg457676/data/Processed_Data_0/pspec0_{:05}.csv".format(i), delimiter = ",")
+
+    j = i % 1000
+    templ = np.genfromtxt("/hpcwork/cg457676/data/parameters/parameters_{}.csv".format(i // 1000), delimiter = ",")[j]
+
+    data[i] = tempd
+    labels[i] = templ
+
+# split
+teSp = int(0.8 * n_data)
+vaSp = int(0.1 * n_data)
+trSP = int(0.1 * n_data)
+
+# Train / Validation / Test - Data
+train_data = data[:teSp]
+valid_data = data[teSp : teSp + vaSp]
+test_data = data[teSp + vaSp:]
+
+train_data = labels[:teSp]
+valid_data = labels[teSp : teSp + vaSp]
+test_data = labels[teSp + vaSp:]
 
 # Creating the model of the CNN
 
