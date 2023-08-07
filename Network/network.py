42571564ae2c927b238b8383e6935bc07b44d897
Andi-00 <andreasweber2001@outlook.de>
2023-08-07 20:42:47 +0200

    start of the CNN

--- Network/network.py
+++ Network/network.py
@@ -2,5 +2,22 @@ from tensorflow import keras
 import numpy as np
 from matplotlib import pyplot as plt
 
-# Input data : shape 79 x 2001 (time x frequency)
+# Input data : shape 2001 x 79 (frequency x time)
+
+# Load the data
+
+data = []
+
+# Number of datasets with n_max = 1E4
+n_data = 10
+
+
+data = np.zeros((n_data, 79, 2001))
+
+for i in range(n_data):
+    temp = np.genfromtxt("/hpcwork/cg457676/data/Processed_Data_0/pspec0_{:05}.csv".format(i), delimiter = ",")
+    data[i] = np.genfromtxt("/hpcwork/cg457676/data/Processed_Data_0/pspec0_{:05}.csv".format(i), delimiter = ",")
+
+# split
+ts = 0.8
 
