e05c6f3663742e0afeb90cc8f37cf7fe779d63f5
Andi-00 <andreasweber2001@outlook.de>
2023-08-25 15:02:41 +0200

    run 7 eval

--- Network/loss.py
+++ Network/loss.py
@@ -26,7 +26,7 @@ plt.rcParams['figure.figsize'] = (10, 6)
 
 
 
-run_number = 6
+run_number = 7
 
 
 model = keras.models.load_model("./Network/network_output/run_1.{:02}/model_1.{:02}.keras".format(run_number, run_number), compile = False)
