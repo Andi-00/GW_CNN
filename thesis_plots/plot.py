799fe3ff75aabcee64a1b57b6dfb770d932f6a4f
Andi-00 <andreasweber2001@outlook.de>
2023-09-01 20:59:05 +0200

    thesis plots

--- thesis_plots/plot.py
+++ thesis_plots/plot.py
@@ -22,20 +22,45 @@ plt.rcParams['savefig.pad_inches'] = 0.1
 #plt.rcParams['savefig.transparent'] = True
 plt.rcParams['figure.figsize'] = (14, 6)
 
+n = 36
 
-parameter = np.genfromtxt("/hpcwork/cg457676/data/parameters/parameters_0.csv", delimiter = ",")
+parameter = np.genfromtxt("/hpcwork/cg457676/data/parameters/parameters_0.csv", delimiter = ",")[n]
+strain = np.genfromtxt("/hpcwork/cg457676/data/strains/h_{:05}.csv".format(n), delimiter = ",", dtype = complex)
+spec = np.swapaxes(np.genfromtxt("/hpcwork/cg457676/data/spectrograms/spec_{:05}.csv".format(n), delimiter = ","), 0, 1)
 
 
-spec = np.swapaxes(np.genfromtxt("/hpcwork/cg457676/data/Processed_Data_0/" + "pspec0_{:05}.csv".format(s + i), delimiter = ","), 0, 1)
+np.savetxt("./thesis_plots/chapter_4/parameter.csv", parameter, delimiter = ",")
+
+# Strain plot
+fig, ax = plt.subplots()
+
+N = 1000
+
+t = 5 * np.arange(N)
+y = strain[: N].real
+
+
+ax.plot(t, y * 1E25, color = "#e60049")
+ax.set_ylabel("Strain $h_+ / 10^{-25}$")
+ax.set_xlabel("Time $t / \mathrm{s}$")
+ax.set_title("Strain of dataset nr. {:04}".format(n), y = 1.02)
+
+ax.grid()
+
+import matplotlib.colors as colors
+
+plt.savefig("./thesis_plots/chapter_4/strain.png")
+
+
+# Plot spectrogram
+fig, ax = plt.subplots()
 
 x = (np.arange(0, 79) + 0.5) * 2E4 / (3600 * 24)
 y = (np.arange(2E3 + 1) + 0.5) * 5E-5
 z = spec
 
 
-fig, ax = plt.subplots()
-
-pc = ax.pcolormesh(x, y, z)
+pc = ax.pcolormesh(x, y, z, norm = colors.LogNorm(vmin = np.max(spec) * 1E-6, vmax = np.max(spec)))
 ax.set_yscale("log")
 
 ax.set_ylim(1E-4, 1E-1)
@@ -43,12 +68,12 @@ ax.set_ylim(1E-4, 1E-1)
 
 ax.set_ylabel("Frequency $f$")
 ax.set_xlabel("Time $t$ in days")
-ax.set_title("{})  log M = {:.2f}, a = {:.2f}".format(s + i, np.log10(parameter[i][0]), parameter[i][2]), y = 1.02)
 # ax.colorbar(label=r'Gravitational wave amplitude [1/$\sqrt{\mathrm{Hz}}$]')
 
 plt.colorbar(pc, label=r'Gravitational wave amplitude [1/$\sqrt{\mathrm{Hz}}$]')
+ax.set_title("Spectrogram of dataset nr. {:04}".format(n), y = 1.02)
 
 
 ax.grid(False)
 
-plt.savefig("./Network/test_programme/specs/spec_{:01}.png".format(s + i))
+plt.savefig("./thesis_plots/chapter_4/spec.png")
